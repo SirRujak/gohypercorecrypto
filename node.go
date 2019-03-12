f0461ea08a3c0fb90371b8652769b39dbc1ee243
Rujak Ironhammer <sirrujak@gmail.com>
2019-03-12 15:45:59 -0500

    Begin describing the Node struct. TODO: Missing the node.Parent init.

--- node.go
+++ node.go
@@ -1,1 +1,18 @@
 package gohypercorecrypto
+
+type Node struct {
+	Index  uint
+	Hash   []uint8
+	Length uint
+	Parent uint
+	Data   *[]uint8
+}
+
+func (node *Node) New(index uint, hash []uint8, length uint) {
+	node.Index = index
+	node.Hash = hash
+	node.Length = length
+	node.Parent = ???
+	var tempData = make([]uint8, 0, 0)
+	node.Data = &tempData
+}
