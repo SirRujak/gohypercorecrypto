73e352115e6ab22a61e77ec347d4ae223f64affe
Rujak Ironhammer <sirrujak@gmail.com>
2019-03-12 15:45:28 -0500

    Finish out most of the hypercore-crypto functions.

--- gohypercorecrypto.go
+++ gohypercorecrypto.go
@@ -69,3 +69,44 @@ func HashLeaf(data []byte) (sum []byte) {
 	h.Write(data)
 	return h.Sum(sum)
 }
+
+func HashParent(left, right Node) (*[]byte, error) {
+	var node1, node2 Node
+	if left.Index <= right.Index {
+		node1 = left
+		node2 = right
+	} else {
+		node1 = right
+		node2 = left
+	}
+
+	var tempBytes []byte
+	binary.BigEndian.PutUint64(tempBytes, uint64(node1.Length+node2.Length))
+
+	var blakeList [][]byte
+	blakeList = make([][]byte, 4)
+	blakeList = append(blakeList, parentType)
+	blakeList = append(blakeList, tempBytes)
+	blakeList = append(blakeList, node1.Hash)
+	blakeList = append(blakeList, node2.Hash)
+	return Blake2bList(blakeList)
+}
+
+func HashTree(roots []Node) (*[]byte, error) {
+	var hashList [][]byte
+	hashList = make([][]byte, 3*len(roots)+1)
+
+	for i := 0; i < len(roots); i++ {
+		hashList = append(hashList, roots[i].Hash)
+		var tempBytesIndex, tempBytesLength []byte
+		tempBytesIndex = make([]byte, 8)
+		tempBytesLength = make([]byte, 8)
+		hashList = append(hashList, binary.BigEndian.PutUint64(tempBytesIndex, uint64(roots[i].Index)))
+		hashList = append(hashList, binary.BigEndian.PutUint64(tempBytesLength, uint64(roots[i].Length)))
+	}
+	return Blake2bList(hashList)
+}
+
+func Data(data []byte) (*[]byte, error) {
+	return Blake2bList([][]byte{data})
+}
